76e411d9d7953affa3d3771822c488d5b633d2ea
dtorresUSB <dtorres@usbbog.edu.co>
2023-08-15 14:26:16 -0500

    mi primer commit

---
+++ Grupo2/Corte1/Sesion3/Calificacion.py
@@ -0,0 +1,9 @@
+nota=float(input('ingrese su nota: '))
+
+if (5>=nota>=0):    
+    if (nota>=3):
+        print('Usted aprobó')
+    else:
+        print('Usted no aprobó')
+else:
+    print('valor ingresado invalido')
