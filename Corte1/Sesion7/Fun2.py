4d90b15c93823f8d7619aeef117de52d32c4b343
dtorresUSB <dtorres@usbbog.edu.co>
2023-08-22 15:12:24 -0500

    actualizacion sesion 7

---
+++ Corte1/Sesion7/Fun2.py
@@ -0,0 +1,9 @@
+#Funcion sin retorno
+def matrix(nombre):
+    print('Estas en la matrix', nombre )
+
+
+print('Inicio del programa')
+n=input('Ingrese su nombre: ')
+matrix(n)
+print('Fin programa')
