a8548922eebf215c382ffec89ed6468403f90aac
dtorresUSB <dtorres@usbbog.edu.co>
2023-10-24 14:25:01 -0500

    actualizacion sesion 18

--- Corte3/Sesion17/Notas.py
+++ Corte3/Sesion17/Notas.py
@@ -1,11 +1,12 @@
 
 class Estudiantes():
-    def __init__(self):     #Método Constructor
-        self.__nombre=None
-        self.__codigo=None
-        self.__carrera=None
-        self.__materia=None
-        self.__notas=None
+    def __init__(self,nombre:str,codigo:int,carrera:str,
+    materia:str,notas:float):     #Método Constructor
+        self.__nombre=nombre
+        self.__codigo=codigo
+        self.__carrera=carrera
+        self.__materia=materia
+        self.__notas=notas
 
     #---------- Setters ------------
     def setNombre(self,nombre:str):
@@ -17,7 +18,8 @@ class Estudiantes():
     def setMateria(self,materia:str):
         self.__materia=materia
     def setNotas(self,notas:float):
-        self.__notas=notas
+        if 0<notas<5:
+            self.__notas=notas
     
     #---------- Getters ------------
     def getNombre(self):
@@ -31,8 +33,45 @@ class Estudiantes():
     def getNotas(self):
         return self.__notas
 
+    #--------- Metodo -----------
+    def __promedio(self):
+        n=self.getNotas()
+        return round(sum(n)/len(n),2)
+
+    def getPromedio(self):
+        average=self.__promedio()
+        if average<3:
+            return f'El estudiante {self.getNombre()} reprobó con {average}'
+        return f'El estudiante {self.getNombre()} aprobó con {average}'
+
 def main():
-    pass
+    while 1:
+        universidad=[]
+        nota=[]
+        nombre=input('Nombre: ')
+        codigo=input('Codigo: ')
+        carrera=input('Carrera: ')
+        materia=input('Materia: ')
+        numero_notas=int(input('Cantidad de notas: '))
+        x=1
+        while x<numero_notas+1:
+            notas=float(input(f'Nota {x}: '))
+            if 0<=notas<=5:
+                nota.append(notas)
+                x+=1
+            else:
+                print('Ingreso una nota invalida')
+        
+        usuario=Estudiantes(nombre,codigo,carrera,materia,nota)
+        universidad.append(usuario)
+
+
+        print(f'\nNombre: {usuario.getNombre()}\n'\
+            f'Codigo: {usuario.getCodigo()}\n'\
+            f'Materia: {usuario.getMateria()}\n'\
+                f'Carrera: {usuario.getCarrera()}\n'\
+                    f'Nota: {usuario.getNotas()}\n'\
+                        f'=> {usuario.getPromedio()}')
 
 if __name__=="__main__":
     main()
